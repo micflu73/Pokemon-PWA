21e9b17d2e4e97a5b746498c098d5c052c8cf26d
Michael <michael.fluch@gmail.com>
2021-01-07 13:24:32 +0100

    Fetching data

---
+++ app.js
@@ -0,0 +1,17 @@
+const pokemonList = document.querySelector("#pokemonList");
+
+window.addEventListener("load", (e) => {
+    getPokemonList();
+});
+
+async function getPokemonList() {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=35");
+    const json = await response.json();
+
+    console.log(json);
+
+    pokemonList.innerHTML = json.results.map(
+        (result) => `<option value="${result.url}">${result.name}</option>`
+    );
+
+}
