86ef53f03df5893277bca4a387f36ca60d79044b
Michael <michael.fluch@gmail.com>
2021-01-08 14:31:47 +0100

    Offline storage

--- sw.js
+++ sw.js
@@ -1,7 +1,19 @@
+const CACHE_DATA = "offline-data";
+const STATIC_RESOURCES = ["index.html", "app.js", "logo.png"];
+
 // Install the SW
 self.addEventListener("install", async (e) => {
     console.log("SW installed");
-})
+
+    e.waitUntil(
+        (async () => {
+            const cache = await caches.open(CACHE_DATA);
+            return await cache.addAll(STATIC_RESOURCES);
+        })()
+    );
+
+    self.skipWaiting();
+});
 
 // Listen for fetching request
 self.addEventListener("fetch", async (e) => {
