c097f58c3c344da26de395b55cbd2d6198e23016
Michael <michael.fluch@gmail.com>
2021-01-09 18:35:22 +0100

    Fetch even listener

--- app.js
+++ app.js
@@ -4,61 +4,77 @@ const defaultPokemon = "https://pokeapi.co/api/v2/pokemon/1/";
 const pokemonCard = document.querySelector("#pokemonCard");
 
 window.addEventListener("load", (e) => {
-    // on load
-    getPokemonList().then(() => {
-        showPokemonCard(defaultPokemon);
-    });
-    // on change of dropdown
-    pokemonList.addEventListener("change", (e) => {
-        showPokemonCard(e.target.value);
-    })  
-    
-    registerServiceWorker();
+	getPokemonList().then(() => {
+		showPokemonCard(defaultPokemon);
+	});
+
+	pokemonList.addEventListener("change", (e) => {
+		showPokemonCard(e.target.value);
+	});
+
+	registerServiceWorker();
 });
 
 async function getPokemonList() {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=35");
-    const json = await response.json();
+	const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=35");
+	const json = await response.json();
 
-    console.log(json);
-
-    pokemonList.innerHTML = json.results.map(
-        (result) => `<option value="${result.url}">${result.name}</option>`
-    );
-
+	pokemonList.innerHTML = json.results.map(
+		(result) => `<option value="${result.url}">${result.name}</option>`
+	);
 }
 
-function createCard(pokemon) {
-    return `
-        <div class="class-header">
-            <h2>#${pokemon.id}</h2>
-        </div>
-        <img src="${pokemon.sprites.other.dream_world.front_default}" 
-            class="card-img-top"
-            width="150"
-            height="150">
-        <div class="card-body">
-            <h5 class="card-title" style="text-transform:capitalize">${pokemon.name}</h5>
-            <div class="badge badge-warning">Height: ${pokemon.height}</div>
-            <div class="badge badge-danger">Weight: ${pokemon.weight}</div>
+async function showPokemonCard(url) {
+	try {
+		const response = await fetch(url);
+		const json = await response.json();
+
+		pokemonCard.innerHTML = createCard(json);
+	} catch (error) {
+		console.log("Network is unavailable");
+		pokemonCard.innerHTML = offlineCard();
+	}
+}
+
+function offlineCard() {
+	return `
+        <div class="card-header">
+            <p>Network is unavailable</p>
         </div>
     `;
 }
 
-async function showPokemonCard(url) {
-    const response = await fetch(url)
-    const json = await response.json();
-
-    pokemonCard.innerHTML = createCard(json);
+function createCard(pokemon) {
+	return `
+    <div class="card-header">
+        <h2>#${pokemon.id}</h2>
+    </div>
+    <img
+        src="${pokemon.sprites.other.dream_world.front_default}"
+        class="card-img-top"
+        width="150"
+		height="150"
+		alt="${pokemon.name}"
+    />
+    <div class="card-body">
+        <h3
+            class="card-title"
+            style="text-transform: capitalize"
+        >
+            ${pokemon.name}
+        </h3>
+        <div class="badge badge-warning">Height: ${pokemon.height}</div>
+        <div class="badge badge-danger">Weight: ${pokemon.weight}</div>
+    </div>
+    `;
 }
 
 async function registerServiceWorker() {
-    if ("serviceWorker" in navigator) {
-        try {
-            await navigator.serviceWorker.register("sw.js");
-        } catch (error) {
-            console.log("Failed: ", error);
-        }
-    }
+	if ("serviceWorker" in navigator) {
+		try {
+			await navigator.serviceWorker.register("sw.js");
+		} catch (error) {
+			console.log("Failed: ", error);
+		}
+	}
 }
-
--- sw.js
+++ sw.js
@@ -18,7 +18,22 @@ self.addEventListener("install", async (e) => {
 // Listen for fetching request
 self.addEventListener("fetch", async (e) => {
     console.log(`SW fetch: ${e.request.url}`);
-})
+
+    e.respondWith(
+        (async () => {
+            const cache = await caches.open(CACHE_DATA);
+
+            try {
+                const networkResponse = await fetch(e.request);
+                await cache.put(e.request, networkResponse.clone());
+                return networkResponse;
+            } catch (error) {
+                const cachedResponse = await cache.match(e.request);
+                return cachedResponse;
+            }
+        })()
+    );
+});
 
 // Activate the SW
 self.addEventListener("activate", async (e) => {
